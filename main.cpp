236f2dda066d12423190b263bab753bba2768188
alferz <josh@fusebin.com>
2017-05-22 16:14:16 +0000

    Updated seednodes to new worldcoin.global domain

--- main.cpp
+++ main.cpp
@@ -339,7 +339,7 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"seednode1.worldcoincore.com", "seednode2.worldcoincore.com", "seednode3.worldcoincore.com", "seednode4.worldcoincore.com", "seednode5.worldcoincore.com", "seednode6.worldcoincore.com", "wdc.theblocksfactory.com", "pool2b.d2.cc", ""};
+static const string mainnet_seeds[] = {"seednode3.worldcoin.global", "seednode2.worldcoin.global", ""};
 static const string testnet_seeds[] = {"54.186.69.238", ""};
 static const string *seeds = mainnet_seeds;
 
